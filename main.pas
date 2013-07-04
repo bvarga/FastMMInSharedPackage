f75104ba36538a177ff32b3fe63804dc740c191f
Balazs Varga <bb.varga@gmail.com>
2013-07-04 15:16:18 +0200

    -Initial files

---
+++ main.pas
@@ -0,0 +1,33 @@
+unit main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+procedure MyInit; stdcall; external 'dll.dll';
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  TStringList.Create;
+  MyInit;
+end;
+
+end.
